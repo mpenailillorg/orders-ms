ecb096ed581f1e2e518e197f06caa80a3b5a72e9
Matias P. <matias.penailillob@gmail.com>
2024-12-03 15:24:42 -0300

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger('Orders')
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+  await app.listen(envs.port);
+  logger.log(`Orders Ms running on port ${ envs.port }`)
+}
+bootstrap();
